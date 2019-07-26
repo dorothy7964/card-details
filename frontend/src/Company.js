1ee266d3cf78a33d3f04648d95a9ba7d40672d58
sample <dorothy_007@naver.com>
2019-07-26 19:28:22 +0900

    move css

--- frontend/src/Company.js
+++ frontend/src/Company.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
-import './company.css';
+import './css/company.css';
 import Card from './Card';
 
 class Company extends Component {
